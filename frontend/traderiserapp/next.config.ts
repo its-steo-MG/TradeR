0897807db914613ca5dc1dd744e702d4151f6aab
its-steo <steomustadd@gmail.com>
2026-02-18 05:05:41 +0300

    add new changes

--- frontend/traderiserapp/next.config.ts
+++ frontend/traderiserapp/next.config.ts
@@ -1,37 +1,32 @@
 // next.config.ts
 import type { NextConfig } from 'next';
-import { LoginType } from './app/fx-pro-robots/page';
-import { Account } from './types/account';
-const withPWA: any = require('next-pwa');
 
 const nextConfig: NextConfig = {
-  // Disable Turbopack, use Webpack (stable)
-  // Remove all "turbo" flags
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'traderiser-storage.s3.amazonaws.com',
         port: '',
-        pathname: '/assets/**', // Restrict to assets folder for security
+        pathname: '/assets/**',
       },
       {
         protocol: 'https',
         hostname: 'traderiser-storage.s3.eu-north-1.amazonaws.com',
         port: '',
-        pathname: '/assets/**', // Include region-specific hostname if used
+        pathname: '/assets/**',
       },
       {
         protocol: 'https',
         hostname: 'grandview-storage.s3.amazonaws.com',
         port: '',
-        pathname: '/agents/**', // Restrict to agents folder for security
+        pathname: '/agents/**',
       },
       {
         protocol: 'https',
         hostname: 'grandview-storage.s3.eu-north-1.amazonaws.com',
         port: '',
-        pathname: '/agents/**', // Include region-specific hostname if used
+        pathname: '/agents/**',
       },
       {
         protocol: 'https',
@@ -43,15 +38,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default withPWA({
-  dest: 'public', // Output directory for the generated service worker
-  disable: process.env.NODE_ENV === 'development', // Disable in development to avoid caching issues
-  register: true, // Automatically register the service worker
-  skipWaiting: true, // Skip waiting on install
-  swSrc: 'app/sw.js', // Path to the custom service worker source file
-  swDest: 'sw.js', // Output filename for the generated service worker (in public/)
-})(nextConfig);
-interface SidebarProps {
-  loginType: LoginType;
-  activeAccount: Account | null;
-}
+export default nextConfig;
