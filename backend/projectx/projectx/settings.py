ef28ac5561b659661dfde4a13b228a691062a1b6
its-steo <steomustadd@gmail.com>
2026-02-18 06:27:26 +0300

    add new chnages

--- backend/projectx/projectx/settings.py
+++ backend/projectx/projectx/settings.py
@@ -18,7 +18,7 @@ DEBUG = True
 ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1',
-    'https://traderiserproapp.onrender.com',
+    'https://traderiserproapp.onrender.com/',
     'https://traderiserproapp.vercel.app/',
 
    
@@ -29,7 +29,7 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    'https://traderiserproapp.onrender.com',
+    'https://traderiserproapp.onrender.com/',
     'https://traderiserproapp.vercel.app/',
    
 ]
@@ -37,7 +37,7 @@ CORS_ALLOWED_ORIGINS = [
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    'https://traderiserproapp.onrender.com',
+    'https://traderiserproapp.onrender.com/',
     'https://traderiserproapp.vercel.app/',
   
 ]
