b6f7f01e46d3addd5e451a46130bf657b58aa776
its-steo <steomustadd@gmail.com>
2026-02-18 06:34:24 +0300

    add new chnages

--- backend/projectx/projectx/settings.py
+++ backend/projectx/projectx/settings.py
@@ -18,8 +18,8 @@ DEBUG = True
 ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1',
-    'https://traderiserproapp.onrender.com/',
-    'https://traderiserproapp.vercel.app/',
+    'https://traderiserproapp.onrender.com',
+    'https://traderiserproapp.vercel.app',
 
    
 ]
@@ -29,16 +29,16 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    'https://traderiserproapp.onrender.com/',
-    'https://traderiserproapp.vercel.app/',
+    'https://traderiserproapp.onrender.com',
+    'https://traderiserproapp.vercel.app',
    
 ]
 # REQUIRED FOR DJANGO ADMIN POST/DELETE FROM FRONTEND
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    'https://traderiserproapp.onrender.com/',
-    'https://traderiserproapp.vercel.app/',
+    'https://traderiserproapp.onrender.com',
+    'https://traderiserproapp.vercel.app',
   
 ]
 
