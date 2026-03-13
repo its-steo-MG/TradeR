793ffc2df5672e57286a10da94f490b7fb6df19a
its-steo <steomustadd@gmail.com>
2026-03-13 18:13:49 +0300

    add new chnages

--- backend/projectx/agents/views.py
+++ backend/projectx/agents/views.py
@@ -17,7 +17,7 @@ from django.urls import reverse
 import logging
 
 logger = logging.getLogger(__name__)
-ADMIN_EMAIL = "globalgrowthinvest@gmail.com"
+ADMIN_EMAIL = "traderiserpro@gmail.com"
 
 class AgentListView(APIView):
     def get(self, request):
